b57c0c4f8e51177cdaf24f7b3be3c5371b9d4c9a
Elvin Hajizada <elvin.hajizada@intel.com>
2023-02-13 12:16:50 -0800

    CLP tracks mistaken protos; uses hits/misses to calc learning rate

--- src/clp/clp.py
+++ src/clp/clp.py
@@ -7,8 +7,7 @@ import torch
 from torchmetrics.functional import pairwise_cosine_similarity
 from torch.nn.functional import one_hot
 from torch.linalg import norm
-
-
+torch.set_printoptions(precision=2)
 
 from avalanche.training.plugins import SupervisedPlugin
 from avalanche.training.templates.supervised import SupervisedTemplate
@@ -27,14 +26,11 @@ class CLP(SupervisedTemplate):
             bmu_metric,
             criterion,
             alpha_start,
-            tau_alpha_decay,
-            tau_alpha_growth,
             sim_th,
-            w_max,
-            w_min,
             input_size,
             num_classes,
             max_allowed_mistakes,
+            verbose = 0,
             output_layer_name=None,
             train_epochs: int = 1,
             train_mb_size: int = 1,
@@ -94,21 +90,22 @@ class CLP(SupervisedTemplate):
         # CLVQ parameters
         self.bmu_metric = bmu_metric
         self.alpha_start = alpha_start
-        self.alpha_dc = np.e ** (-1 / tau_alpha_decay)
-        self.alpha_gr = np.e ** (-1 / tau_alpha_growth)
         self.max_allowed_mistakes = max_allowed_mistakes
         self.num_classes = num_classes
         self.sim_th = sim_th
-        self.w_max = w_max
-        self.w_min = w_min
+        self.verbose = verbose
         
         # setup weights for CLVQ
         self.n_protos = n_protos
-        self.prototypes = torch.zeros(n_protos, input_size).to(device)
+        self.prototypes = torch.zeros(n_protos, input_size).to(self.device)
         self.proto_labels = num_classes * torch.ones((n_protos, 1), dtype=int)
-        self.alphas = torch.ones((n_protos, 1)).to(self.device)
+        self.alphas = self.alpha_start*torch.ones((n_protos, 1)).to(self.device)
+        self.hits = torch.ones((n_protos, 1)).to(self.device)
+        self.misses = torch.ones((n_protos, 1)).to(self.device)
         self.n_alc_bc_miss = 0    # Num of allocated protos because of errors
         self.sims = []
+        self.seen_labels = []
+        self.mistaken_proto_inds = [] # inds of protos that made incorrect inferences for current sample
 
     def forward(self, return_features=False):
         """Compute the model's output given the current mini-batch."""
@@ -171,12 +168,20 @@ class CLP(SupervisedTemplate):
         y = torch.tensor(int(y))
         while True:
             
-            bmu_ind = self.get_best_matching_unit(x)
+            bmu_ind = self.get_best_matching_unit(x).item()
             
+            if y.item() not in self.seen_labels:
+                self.seen_labels.append(y.item())
+                print("Novel Label!")
+                self._allocate(x, y)
+                break
+                
             # Novel instance --> Allocate
             # if no winner, because all similarities are below the given threshold, then allocate
             if bmu_ind == -1:
+                print("Novel Instance!")
                 self._allocate(x, y)
+                self.mistaken_proto_inds = []  # reset mistake buffer
                 break
             
             # # Novel label --> Allocate
@@ -202,23 +207,28 @@ class CLP(SupervisedTemplate):
             # If winner not assigned to a label, then assign it to
             # the training instance's label
             if self.proto_labels[bmu_ind] == self.num_classes:
-                # print("Unsupervised llocating...")
+                print("Unsupervised allocating...")
                 mistake = False
                 self.proto_labels[bmu_ind] = y
                 self.prototypes[bmu_ind] += self.alphas[bmu_ind] * error
-                self.alphas[bmu_ind] = self.alpha_dc * self.alphas[bmu_ind]
+                self.hits[bmu_ind]+=1
+                self.alphas[bmu_ind] = self.misses[bmu_ind]/self.hits[bmu_ind]
                 # self._bound_weights(bmu_ind)
+                self.mistaken_proto_inds = []
                 break
 
             # Update the winner based on its inference
             # If CORRECT prediction
             elif self.proto_labels[bmu_ind] == y:
                 mistake = False
-                # print("Correct")
+                if self.verbose == 1:
+                    print("Correct")
                 # print(self.alphas[bmu_ind])
                 self.prototypes[bmu_ind] += self.alphas[bmu_ind] * error
-                self.alphas[bmu_ind] = self.alpha_dc * self.alphas[bmu_ind]
+                self.hits[bmu_ind]+=0.7
+                self.alphas[bmu_ind] = self.misses[bmu_ind]/self.hits[bmu_ind]
                 # self._bound_weights(bmu_ind)
+                self.mistaken_proto_inds = []
                 break
 
             # if INCORRECT prediction 
@@ -226,10 +236,18 @@ class CLP(SupervisedTemplate):
                 # print("Mistake")
                 mistake = True
                 n_mistakes += 1
+                print("Mistaken prototype:", self.proto_labels[bmu_ind], bmu_ind)
+                self.mistaken_proto_inds.append(bmu_ind)
                 self.prototypes[bmu_ind] -= self.alphas[bmu_ind] * error
-                self.alphas[bmu_ind] = min(
-                        (self.alphas[bmu_ind] * self.alpha_gr),
-                        self.alpha_start)
+                self.misses[bmu_ind] += 5
+                self.alphas[bmu_ind] = self.misses[bmu_ind]/self.hits[bmu_ind]
+                
+                # If more misses than hits, then forget this prototype, reset it
+                if self.alphas[bmu_ind] > 1: 
+                    self.proto_labels[bmu_ind] = self.num_classes
+                    self.alphas[bmu_ind] = self.alpha_start
+                    self.hits[bmu_ind], self.misses[bmu_ind] = 1, 1
+                    
                 
                 # self._bound_weights(bmu_ind)
 
@@ -241,19 +259,25 @@ class CLP(SupervisedTemplate):
                 # Allocate a new non-winning prototype if maximum number of allowed
                 # mistakes are passed
                 elif n_mistakes == self.max_allowed_mistakes:
+                    print("Ignoring the instance")
                     self.n_alc_bc_miss += 1
-                    self._allocate(x, y)
+                    self.mistaken_proto_inds = []
+                    # self._allocate(x, y)
                     break
     
     def _allocate (self, x, y):
         # print("Mistake again, allocating...")
         similarities = self._calc_similarities(x)
         similarities[self.proto_labels<self.num_classes] -= 100000
+        
         bmu_ind = torch.argmax(similarities, dim=0)
         self.proto_labels[bmu_ind] = y
+        
         error = x - self.prototypes[bmu_ind]
         self.prototypes[bmu_ind] += self.alphas[bmu_ind] * error
-        self.alphas[bmu_ind] = self.alpha_dc * self.alphas[bmu_ind]
+        
+        self.hits[bmu_ind]+=1
+        self.alphas[bmu_ind] = self.misses[bmu_ind]/self.hits[bmu_ind]
         # self._bound_weights(bmu_ind)
     
     def _bound_weights(self, bmu_ind):
@@ -295,12 +319,19 @@ class CLP(SupervisedTemplate):
         ind = 0
         
         similarities = self._calc_similarities(x)
+        sims = similarities.clone().detach()
+        top_sims, top_inds = torch.sort(sims, 0, descending=True)
+        top_sims, top_inds = top_sims[:8,0], top_inds[:8,0]
+        # print(top_sims.shape, top_inds.shape)
+        if self.verbose == 1:
+            print("-----------------------------------------------------------")
+            print("sims:  ",top_sims.t().data)
+            print("labels:",self.proto_labels[top_inds].t().data)
+            print("alphas:",self.alphas[top_inds].t().data)
         
+        similarities[self.mistaken_proto_inds] -= 10000
         (max_sim, bmu_inds) = torch.max(similarities, dim=0, keepdim=False)
-        self.sims.append(max_sim)
-        
-        bmu_inds[torch.count_nonzero(max_sim>self.sim_th) == 0] = -1
-        
+        bmu_inds[(max_sim>self.sim_th) == 0] = -1
         return bmu_inds
             
 #         if self.bmu_metric == 'euclidean':
@@ -363,89 +394,4 @@ class CLP(SupervisedTemplate):
     def criterion(self):
         """Loss function."""
         return 0
-
-    def fit_base(self, X, y):
-        """
-        Fit the SLDA model to the base data.
-        :param X: an Nxd torch tensor of base initialization data
-        :param y: an Nx1-dimensional torch tensor of the associated labels for X
-        :return: None
-        """
-        print("\nFitting Base...")
-
-        # update class means
-        for k in torch.unique(y):
-            self.muK[k] = X[y == k].mean(0)
-            self.cK[k] = X[y == k].shape[0]
-        self.num_updates = X.shape[0]
-
-        print("\nEstimating initial covariance matrix...")
-        from sklearn.covariance import OAS
-
-        cov_estimator = OAS(assume_centered=True)
-        cov_estimator.fit((X - self.muK[y]).cpu().numpy())
-        self.Sigma = (
-                torch.from_numpy(cov_estimator.covariance_).float().to(
-                    self.device)
-        )
-
-    def save_model(self, save_path, save_name):
-        """
-        Save the model parameters to a torch file.
-        :param save_path: the path where the model will be saved
-        :param save_name: the name for the saved file
-        :return:
-        """
-        # grab parameters for saving
-        d = dict()
-        d["muK"] = self.muK.cpu()
-        d["cK"] = self.cK.cpu()
-        d["Sigma"] = self.Sigma.cpu()
-        d["num_updates"] = self.num_updates
-
-        # save model out
-        torch.save(d, os.path.join(save_path, save_name + ".pth"))
-
-    def load_model(self, save_path, save_name):
-        """
-        Load the model parameters into StreamingLDA object.
-        :param save_path: the path where the model is saved
-        :param save_name: the name of the saved file
-        :return:
-        """
-        # load parameters
-        d = torch.load(os.path.join(save_path, save_name + ".pth"))
-        self.muK = d["muK"].to(self.device)
-        self.cK = d["cK"].to(self.device)
-        self.Sigma = d["Sigma"].to(self.device)
-        self.num_updates = d["num_updates"]
-
-    def _check_plugin_compatibility(self):
-        """Check that the list of plugins is compatible with the template.
-
-        This means checking that each plugin impements a subset of the
-        supported callbacks.
-        """
-        # I don't know if it's possible to do it in Python.
-        ps = self.plugins
-
-        def get_plugins_from_object(obj):
-            def is_callback(x):
-                return x.startswith("before") or x.startswith("after")
-
-            return filter(is_callback, dir(obj))
-
-        cb_supported = set(get_plugins_from_object(self.PLUGIN_CLASS))
-        cb_supported.remove("before_backward")
-        cb_supported.remove("after_backward")
-        for p in ps:
-            cb_p = set(get_plugins_from_object(p))
-
-            if not cb_p.issubset(cb_supported):
-                warnings.warn(
-                        f"Plugin {p} implements incompatible callbacks for template"
-                        f" {self}. This may result in errors. Incompatible "
-                        f"callbacks: {cb_p - cb_supported}",
-                )
-                return
 
